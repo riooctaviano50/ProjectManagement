8c0fe58c333fecdd17c66f54f7bf758ded503c16
dewiapril1996@gmail.com <dewiapril1996@gmail.com>
2019-06-10 13:52:49 +0700

    create model Employee.cs create model mv EmployeeVM.cs create my Context/MyContext.cs Create App.config

---
+++ ProjectManagement_WPF/DataAccess/Context/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("MyContext") { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
