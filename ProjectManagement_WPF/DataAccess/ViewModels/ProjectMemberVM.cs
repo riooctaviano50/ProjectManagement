a068d000d36ad00c0a6b35f069dc6df05c782043
RIO\Rio <riooctaviano50@gmail.com>
2019-06-11 10:03:47 +0700

    merge all model and create database

--- ProjectManagement_WPF/DataAccess/ViewModels/ProjectMemberVM.cs
+++ ProjectManagement_WPF/DataAccess/ViewModels/ProjectMemberVM.cs
@@ -10,14 +10,14 @@ namespace DataAccess.ViewModels
     {
         public int Id { get; set; }
         public int Project_Id { get; set; }
-        public int Role_Id { get; set; }
+        public int Rules_Id { get; set; }
         public int User_Id { get; set; }
 
         public ProjectMemberVM(int Id, int Project_Id, int Role_Id, int User_Id)
         {
             this.Id = Id;
             this.Project_Id = Project_Id;
-            this.Role_Id = Role_Id;
+            this.Rules_Id = Role_Id;
             this.User_Id = User_Id;
         }
         public ProjectMemberVM() { }
@@ -25,7 +25,7 @@ namespace DataAccess.ViewModels
         {
             this.Id = Id;
             this.Project_Id = Project_Id;
-            this.Role_Id = Role_Id;
+            this.Rules_Id = Role_Id;
             this.User_Id = User_Id;
         }
     }
