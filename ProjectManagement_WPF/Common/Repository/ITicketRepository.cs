e2a2a6554c4430c1016ab51b6244bdeba7908936
dewiapril1996@gmail.com <dewiapril1996@gmail.com>
2019-06-13 11:21:43 +0700

    Kerjaan Yuki

---
+++ ProjectManagement_WPF/Common/Repository/ITicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using DataAccess.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Repository
+{
+    public interface ITicketRepository
+    {
+        List<Ticket> Get();
+        Ticket Get(int id);
+        bool Insert(TicketVM ticketVM);
+        bool Delete(int id);
+    }
+}
