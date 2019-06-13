a934c78da53ad5c6e2e7ebcab370a7108274812d
dewiapril1996@gmail.com <dewiapril1996@gmail.com>
2019-06-13 10:30:48 +0700

    create UI Project Detail Overview

---
+++ ProjectManagement_WPF/ProjectManagement_WPF/MenuPM/TabMenuProjectDetailPM/TabProjectDetailMember.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectManagement_WPF.MenuPM.TabMenuProjectDetailPM
+{
+    /// <summary>
+    /// Interaction logic for TabProjectDetailMember.xaml
+    /// </summary>
+    public partial class TabProjectDetailMember : UserControl
+    {
+        public TabProjectDetailMember()
+        {
+            InitializeComponent();
+        }
+
+        }
+}
